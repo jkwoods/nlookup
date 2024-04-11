e601e4dd5712ef35d21c5747976ef02440160975
Sofia Wawrzyniak <fiawawrz@seas.upenn.edu>
2024-04-11 13:41:39 -0400

    fix: revise arkworks code

--- src/circuit.rs
+++ src/circuit.rs
@@ -1,21 +1,7 @@
 use ark_ff::{fields::PrimeField, Zero};
 use ark_relations::r1cs::{ConstraintSynthesizer, ConstraintSystem, ConstraintSystemRef, SynthesisError};
 use ark_std::marker::PhantomData;
-
-type G1 = ark_ec::ProjectiveCurve;
-type G2 = ark_ec::ProjectiveCurve;
-type C1 = NlookupCircuit<<G1 as Group>::Scalar>;
-type C2 = TrivialTestCircuit<<G2 as Group>::Scalar>;
-
-use nova_ark::traits::{
-    circuit::{StepCircuit, TrivialTestCircuit},
-    Group,
-};
-use nova_ark::CompressedSNARK;
-use nova_ark::PublicParams;
-use nova_ark::RecursiveSNARK;
-use nova_ark::StepCounterType;
-use nova_ark::FINAL_EXTERNAL_COUNTER;
+use ark_pallas::Fr as ScalarField;
 
 #[derive(Clone, Debug)]
 pub struct NlookupCircuit<F: PrimeField> {
@@ -46,79 +32,29 @@ impl<F: PrimeField> ConstraintSynthesizer<F> for NlookupCircuit<F> {
 
 #[cfg(test)]
 mod tests {
-
-    use crate::circuit::*;
+    use super::*;
+    use ark_relations::r1cs::{ConstraintSystem, ConstraintSystemRef};
+    use ark_ec::PairingEngine;
+    use ark_ff::Field;
+    use ark_std::rand::Rng;
+    use ark_std::test_rng;
 
     #[test]
     fn test_1() {
-        let circuit_primary: NlookupCircuit<<G1 as Group>::Scalar> = NlookupCircuit::new(0);
+        let rng = &mut test_rng();
+        let circuit = NlookupCircuit::new(0);
+        test_circuit::<_, _, ark_ec::bls12::Bls12_381>(circuit, rng);
+    }
 
-        // trivial circuit
-        let circuit_secondary = TrivialTestCircuit::new(StepCounterType::External);
+    fn test_circuit<E: PairingEngine, C: ConstraintSynthesizer<E::Fr>, R: Rng>(circuit: C, rng: &mut R) {
+        // Create an instance of our circuit (with the witness)
+        let cs = ConstraintSystem::<E::Fr>::new_ref();
+        circuit.generate_constraints(cs.clone()).unwrap();
 
-        // produce public parameters
-        let pp = PublicParams::<
-            G1,
-            G2,
-            NlookupCircuit<<G1 as Group>::Scalar>,
-            TrivialTestCircuit<<G2 as Group>::Scalar>,
-        >::setup(circuit_primary.clone(), circuit_secondary.clone())
-        .unwrap();
+        // Check that the circuit is satisfied
+        assert!(cs.is_satisfied().unwrap());
 
-        println!(
-            "Number of constraints (primary circuit): {}",
-            pp.num_constraints().0
-        );
-        println!(
-            "Number of constraints (secondary circuit): {}",
-            pp.num_constraints().1
-        );
-
-        println!(
-            "Number of variables (primary circuit): {}",
-            pp.num_variables().0
-        );
-        println!(
-            "Number of variables (secondary circuit): {}",
-            pp.num_variables().1
-        );
-
-        let z0_primary = vec![
-            <G1 as Group>::Scalar::zero(),
-            <G1 as Group>::Scalar::from(4),
-        ];
-        let z0_secondary = vec![<G2 as Group>::Scalar::zero()];
-
-        let mut recursive_snark = None;
-
-        // prover folds
-        for i in 0..3 {
-            println!("Proving step {}", i);
-
-            let result = RecursiveSNARK::prove_step(
-                &pp,
-                recursive_snark,
-                circuit_primary.clone(),
-                circuit_secondary.clone(),
-                z0_primary.clone(),
-                z0_secondary.clone(),
-            );
-
-            recursive_snark = Some(result.unwrap());
-        }
-
-        assert!(recursive_snark.is_some());
-        let recursive_snark = recursive_snark.unwrap();
-
-        // prover compresses final SNARK
-        let res = CompressedSNARK::<_, _, _, _, S1, S2>::prove(&pp, &recursive_snark);
-
-        assert!(res.is_ok());
-        let compressed_snark = res.unwrap();
-
-        // verify
-        let res = compressed_snark.verify(&pp, FINAL_EXTERNAL_COUNTER, z0_primary, z0_secondary);
-
-        assert!(res.is_ok());
+        // Check that the number of constraints is correct
+        assert_eq!(cs.num_constraints(), 0);
     }
 }
