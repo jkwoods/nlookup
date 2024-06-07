a36db69794ba7254bdd5fe5faedc4a6823eb78b7
jkwoods <woodsjk@seas.upenn.edu>
2024-06-07 15:31:37 -0400

    arkworks sumcheck

--- src/circuit.rs
+++ src/circuit.rs
@@ -1,37 +1,13 @@
-type G1 = pasta_curves::pallas::Point;
-type G2 = pasta_curves::vesta::Point;
-type C1 = NlookupCircuit<<G1 as Group>::Scalar>;
-type C2 = TrivialTestCircuit<<G2 as Group>::Scalar>;
-type EE1 = nova_snark::provider::ipa_pc::EvaluationEngine<G1>;
-type EE2 = nova_snark::provider::ipa_pc::EvaluationEngine<G2>;
-type S1 = nova_snark::spartan::RelaxedR1CSSNARK<G1, EE1>;
-type S2 = nova_snark::spartan::RelaxedR1CSSNARK<G2, EE2>;
-
-use ::bellperson::{
-    gadgets::num::AllocatedNum, ConstraintSystem, LinearCombination, Namespace, SynthesisError,
-    Variable,
-};
-use ff::{Field, PrimeField};
-use neptune::{
-    circuit2::Elt,
-    poseidon::PoseidonConstants,
-    sponge::api::{IOPattern, SpongeAPI, SpongeOp},
-    sponge::circuit::SpongeCircuit,
-    sponge::vanilla::{Mode, SpongeTrait},
-};
-
-use nova_snark::{
-    traits::{
-        circuit::{StepCircuit, TrivialTestCircuit},
-        Group,
-    },
-    CompressedSNARK, PublicParams, RecursiveSNARK, StepCounterType, FINAL_EXTERNAL_COUNTER,
-};
+use ark_ff::{fields::PrimeField, Zero};
+use ark_relations::r1cs::{ConstraintSynthesizer, ConstraintSystem, ConstraintSystemRef, SynthesisError};
+use ark_std::marker::PhantomData;
+use ark_pallas::Fr as ScalarField;
 
 #[derive(Clone, Debug)]
 pub struct NlookupCircuit<F: PrimeField> {
     placeholder: F,
     batch_size: usize,
+    _engine: PhantomData<F>,
 }
 
 impl<F: PrimeField> NlookupCircuit<F> {
@@ -39,174 +15,46 @@ impl<F: PrimeField> NlookupCircuit<F> {
         return NlookupCircuit {
             placeholder: F::zero(),
             batch_size: batch_size,
+            _engine: PhantomData,
         };
     }
-
-    fn internal_gadget_example<CS>(&self, cs: &mut CS) -> Result<AllocatedNum<F>, SynthesisError>
-    where
-        CS: ConstraintSystem<F>,
-    {
-        let allocated_witness =
-            AllocatedNum::alloc(cs.namespace(|| "unique_name"), || Ok(self.placeholder))?;
-
-        return Ok(allocated_witness);
-    }
 }
 
-impl<F: PrimeField> StepCircuit<F> for NlookupCircuit<F> {
-    // nova wants this to return the "output" of each step
-    fn synthesize<CS>(
-        &self,
-        cs: &mut CS,
-        z: &[AllocatedNum<F>],
-    ) -> Result<Vec<AllocatedNum<F>>, SynthesisError>
-    where
-        CS: ConstraintSystem<F>,
-        G1: Group<Base = <G2 as Group>::Scalar>,
-        G2: Group<Base = <G1 as Group>::Scalar>,
-    {
-        // inputs
-        let alloc_z0 = z[0].clone();
-        let alloc_z1 = z[1].clone();
-
-        let alloc_placeholder = self.internal_gadget_example(cs)?;
-
-        cs.enforce(
-            || format!("z0 * (z1 + 1) == placeholder"),
-            |lc| lc + alloc_z0.get_variable(),
-            |lc| lc + alloc_z1.get_variable() + CS::one(),
-            |lc| lc + alloc_placeholder.get_variable(),
-        );
-
-        // outputs
-        let out = vec![alloc_z0, alloc_z1];
-        return Ok(out);
+impl<F: PrimeField> ConstraintSynthesizer<F> for NlookupCircuit<F> {
+    fn generate_constraints(self, cs: ConstraintSystemRef<F>) -> Result<(), SynthesisError> {
+        // Here you can add your constraints to the ConstraintSystem `cs`
+        // For example:
+        // let var = cs.new_witness_variable(|| Ok(self.placeholder))?;
+        // cs.enforce_constraint(lc!() + var, lc!() + CS::one(), lc!() + var)?;
+        Ok(())
     }
-
-    // arity = len of i/o vector z
-    fn arity(&self) -> usize {
-        return 2;
-    }
-
-    // this function does not make constraints, but you have to have it (stupid)
-    // it's usually used as a sanity check that your synthesize() is outputing what you expect
-    fn output(&self, z: &[F]) -> Vec<F> {
-        assert!(z.len() == 2);
-        return z.to_vec(); // right now the input == output, this should change eventually
-    }
-
-    // can leave this as is
-    fn get_counter_type(&self) -> StepCounterType {
-        StepCounterType::External
-    }
-}
-
-// external helper functions/gadgets
-
-// ret = if condition, a, else b
-// condition must be asserted to be 0 or 1 somewhere else
-fn select<F, CS>(
-    cs: &mut CS,
-    cond: AllocatedNum<F>,
-    a: AllocatedNum<F>,
-    b: AllocatedNum<F>,
-    tag: &String,
-) -> Result<AllocatedNum<F>, SynthesisError>
-where
-    F: PrimeField,
-    CS: ConstraintSystem<F>,
-{
-    let ret = AllocatedNum::alloc(cs.namespace(|| format!("ret {}", tag)), || {
-        Ok(b.get_value().unwrap()
-            + cond.get_value().unwrap() * (a.get_value().unwrap() - b.get_value().unwrap()))
-    })?;
-
-    // RET = B + COND * (A - B) <- ite
-    cs.enforce(
-        || format!("ite {}", tag),
-        |lc| lc + a.get_variable() - b.get_variable(),
-        |lc| lc + cond.get_variable(),
-        |lc| lc + ret.get_variable() - b.get_variable(),
-    );
-
-    return Ok(ret);
 }
 
 #[cfg(test)]
 mod tests {
-
-    use crate::circuit::*;
+    use super::*;
+    use ark_relations::r1cs::{ConstraintSystem, ConstraintSystemRef};
+    use ark_ec::PairingEngine;
+    use ark_ff::Field;
+    use ark_std::rand::Rng;
+    use ark_std::test_rng;
 
     #[test]
     fn test_1() {
-        let circuit_primary: NlookupCircuit<<G1 as Group>::Scalar> = NlookupCircuit::new(0);
+        let rng = &mut test_rng();
+        let circuit = NlookupCircuit::new(0);
+        test_circuit::<_, _, ark_ec::bls12::Bls12_381>(circuit, rng);
+    }
 
-        // trivial circuit
-        let circuit_secondary = TrivialTestCircuit::new(StepCounterType::External);
+    fn test_circuit<E: PairingEngine, C: ConstraintSynthesizer<E::Fr>, R: Rng>(circuit: C, rng: &mut R) {
+        // Create an instance of our circuit (with the witness)
+        let cs = ConstraintSystem::<E::Fr>::new_ref();
+        circuit.generate_constraints(cs.clone()).unwrap();
 
-        // produce public parameters
-        let pp = PublicParams::<
-            G1,
-            G2,
-            NlookupCircuit<<G1 as Group>::Scalar>,
-            TrivialTestCircuit<<G2 as Group>::Scalar>,
-        >::setup(circuit_primary.clone(), circuit_secondary.clone())
-        .unwrap();
+        // Check that the circuit is satisfied
+        assert!(cs.is_satisfied().unwrap());
 
-        println!(
-            "Number of constraints (primary circuit): {}",
-            pp.num_constraints().0
-        );
-        println!(
-            "Number of constraints (secondary circuit): {}",
-            pp.num_constraints().1
-        );
-
-        println!(
-            "Number of variables (primary circuit): {}",
-            pp.num_variables().0
-        );
-        println!(
-            "Number of variables (secondary circuit): {}",
-            pp.num_variables().1
-        );
-
-        let z0_primary = vec![
-            <G1 as Group>::Scalar::zero(),
-            <G1 as Group>::Scalar::from(4),
-        ];
-        let z0_secondary = vec![<G2 as Group>::Scalar::zero()];
-
-        let mut recursive_snark = None;
-
-        // prover folds
-        for i in 0..3 {
-            println!("Proving step {}", i);
-
-            let result = RecursiveSNARK::prove_step(
-                &pp,
-                recursive_snark,
-                circuit_primary.clone(),
-                circuit_secondary.clone(),
-                z0_primary.clone(),
-                z0_secondary.clone(),
-            );
-
-            recursive_snark = Some(result.unwrap());
-        }
-
-        assert!(recursive_snark.is_some());
-        let recursive_snark = recursive_snark.unwrap();
-
-        // prover compresses final SNARK
-        let res = CompressedSNARK::<_, _, _, _, S1, S2>::prove(&pp, &recursive_snark);
-
-        assert!(res.is_ok());
-        let compressed_snark = res.unwrap();
-
-        // verify
-        let res = compressed_snark.verify(&pp, FINAL_EXTERNAL_COUNTER, z0_primary, z0_secondary);
-
-        assert!(res.is_ok());
+        // Check that the number of constraints is correct
+        assert_eq!(cs.num_constraints(), 0);
     }
 }
--- src/sumcheck.rs
+++ src/sumcheck.rs
@@ -1,78 +1,160 @@
-use crate::backend::nova::int_to_ff;
-use ::bellperson::{
-    gadgets::num::AllocatedNum, ConstraintSystem, LinearCombination, Namespace, SynthesisError,
-    Variable,
+use ark_ff::PrimeField;
+use ark_r1cs_std::{
+    alloc::{AllocVar, AllocationMode},
+    boolean::Boolean,
+    eq::EqGadget,
+    fields::{fp::FpVar, FieldVar},
+    R1CSVar, ToConstraintFieldGadget,
 };
-use ff::PrimeField;
+use ark_relations::{
+    lc, ns,
+    r1cs::{ConstraintSynthesizer, ConstraintSystemRef, SynthesisError, Variable},
+};
+use ark_std::test_rng;
 
-pub fn sum_check_circuit<F: PrimeField, CS>(
-    cs: &mut CS,
-    init_claim: AllocatedNum<F>,
-    num_rounds: usize,
-    id: &str,
-) -> Result<Vec<AllocatedNum<F>>, SynthesisError>
-where
-    CS: ConstraintSystem<F>,
-{
-    // current claim in each round
-    let mut claim = init_claim.clone();
+#[derive(Clone, Debug)]
+pub struct SumCheck<F: PrimeField> {
+    ell: usize,
+    poly: Vec<(F, F, F)>, // const, x, x^2
+    table_t: Vec<F>,
+    table_eq: Vec<F>,
+}
 
-    // vector that will store the output values of the sum check circuit
-    let mut output = vec![];
+impl<F: PrimeField> SumCheck<F> {
+    pub fn new(table_t: Vec<F>, q: Vec<usize>, v: Vec<F>) -> Self {
+        let ell = logmn(table_t.len());
+        assert_eq!(q.len(), v.len());
 
-    for j in 1..=num_rounds {
-        // For each round j, we allocate a new variable g_j representing the claim of the current round
-        // We use AllocatedNum::alloc to allocate the variable and provide a closure that returns the value of the current claim
+        let poly = vec![]; // TODO
+        let rs = vec![];
 
-        // A, B, C -> these should get their actual values from prover computations
-// First, we need to get the allocated numbers corresponding to the variables
-        let g_j_const = AllocatedNum::alloc(cs.namespace(|| format!("{}_sc_g_{}_const", id, j)), || Ok(g_j.get_value().unwrap()))?;
-        let g_j_x = AllocatedNum::alloc(cs.namespace(|| format!("{}_sc_g_{}_x", id, j)), || Ok(g_j.get_value().unwrap()))?;
-        let g_j_xsq = AllocatedNum::alloc(cs.namespace(|| format!("{}_sc_g_{}_xsq", id, j)), || Ok(g_j.get_value().unwrap()))?;
+        let table_eq = gen_eq_table(rs, qs, last_q);
 
-        // We enforce a constraint that checks the consistency of the claim between round
-        // claim (either is init_claim or was set last loop to be g_j-1(r_j-1)) == g(0) + g(1) ==
-        // xsq_coeff + x_coeff + const + const
-        let claim_check = cs.enforce(
-            || format!("{}_claim_check_{}", id, j),
-            |lc| lc + claim.get_variable(),
-            |lc| lc + CS::one(),
-            |lc| lc + g_j.get_variable(), // make this a different variable that desribes
-                                          // constraints A + B + C + C
-        );
-
-        let r_j = AllocatedNum::alloc(
-            cs.namespace(|| format!("{}_sc_r_{}", id, j)),
-            || Ok(Fr::random(&mut thread_rng())),
-        )?;
-
-        
-        // Then, we construct the polynomial using the allocated numbers
-        // horner's g_j(r_j) -> (((xsq * r) + x)*r + const)
-        let claim = g_j_const
-            .add(cs.namespace(|| format!("{}_claim_add", id, j)), &r_j.mul(cs.namespace(|| format!("{}_claim_mul", id, j)), &(g_j_x.add(cs.namespace(|| format!("{}_claim_add", id, j)), &r_j.mul(cs.namespace(|| format!("{}_claim_mul", id, j)), &g_j_xsq))))?;
-
-        // If it's the last round, we allocate a variable for the final claim and push it to the out vector
-        if j == num_rounds {
-            // output last g_v(r_v) claim
-            let last_claim = claim.clone();
-            
-            // Add last_claim to the list of public inputs
-            cs.set_input(|| format!("{}_sc_last_claim", id), || Ok(last_claim.get_value().unwrap()));
+        SumCheck {
+            ell,
+            poly,
+            table_t,
+            table_eq,
         }
     }
 
-    Ok(output)
+    pub fn make_circuit(
+        &mut self,
+        cs: ConstraintSystemRef<F>,
+        init_claim: FpVar<F>,
+        num_rounds: usize,
+        id: &str,
+    ) -> Result<FpVar<F>, SynthesisError> {
+        // current claim in each round
+        let mut claim = init_claim.clone();
+
+        for j in 0..num_rounds {
+            // For each round j, we allocate a new variable g_j representing the claim of the current round
+            // We use AllocatedNum::alloc to allocate the variable and provide a closure that returns the value of the current claim
+
+            // A, B, C -> these should get their actual values from prover computations
+            // First, we need to get the allocated numbers corresponding to the variables
+            let g_j_const = FpVar::<F>::new_witness(cs.clone(), || Ok(self.poly[j].0))?;
+            let g_j_x = FpVar::<F>::new_witness(cs.clone(), || Ok(self.poly[j].1))?;
+            let g_j_xsq = FpVar::<F>::new_witness(cs.clone(), || Ok(self.poly[j].2))?;
+
+            // We enforce a constraint that checks the consistency of the claim between round
+            // claim (either is init_claim or was set last loop to be g_j-1(r_j-1)) == g(0) + g(1) ==
+            // xsq_coeff + x_coeff + const + const
+
+            claim.enforce_equal(&(&g_j_const + &g_j_const + &g_j_x + &g_j_xsq))?;
+
+            let r_j = FpVar::<F>::new_witness(cs.clone(), || Ok(F::one()))?;
+
+            // Then, we construct the polynomial using the allocated numbers
+            // horner's g_j(r_j) -> (((xsq * r) + x)*r + const)
+            claim = ((&g_j_xsq * &r_j) + &g_j_x) * &r_j + &g_j_const;
+
+            // If it's the last round, we allocate a variable for the final claim and push it to the out vector
+        }
+
+        Ok(claim)
+    }
+
+    // starts with evals on hypercube
+    fn prover_msg<F: PrimeField>(
+        &self,
+        //    table_t: &mut Vec<Integer>,
+        //    table_eq: &mut Vec<Integer>,
+        //    ell: usize,
+        i: usize,
+        //    sponge: &mut Sponge<F, typenum::U4>,
+    ) -> (F, F, F, F) {
+        let base: usize = 2;
+        let pow: usize = base.pow((ell - i) as u32);
+        //assert_eq!(table_t.len(), base.pow(ell as u32));
+        //assert_eq!(table_eq.len(), base.pow(ell as u32));
+
+        let mut xsq = F::zero();
+        let mut x = F::zero();
+        let mut con = F::zero();
+
+        for b in 0..pow {
+            let ti_0 = &self.table_t[b];
+            let ti_1 = &self.table_t[b + pow];
+            let ei_0 = &self.table_eq[b];
+            let ei_1 = &self.table_eq[b + pow];
+
+            let t_slope = ti_1 - ti_0;
+            let e_slope = ei_1 - ei_0;
+
+            xsq += t_slope * &e_slope;
+            x += e_slope * ti_0;
+            x += t_slope * ei_0;
+            con += ti_0 * ei_0;
+        }
+        // todo V
+        let query = vec![
+            int_to_ff(con.clone()),
+            int_to_ff(x.clone()),
+            int_to_ff(xsq.clone()),
+        ];
+
+        let acc = &mut ();
+        SpongeAPI::absorb(sponge, 3, &query, acc);
+        let rand = SpongeAPI::squeeze(sponge, 1, acc);
+        let r_i = Integer::from_digits(rand[0].to_repr().as_ref(), Order::Lsf);
+
+        for b in 0..pow {
+            table_t[b] = &table_t[b] * (F::one() - &r_i) + &table_t[b + pow] * &r_i;
+            table_eq[b] = &table_eq[b] * (F::one() - &r_i) + &table_eq[b + pow] * &r_i;
+        }
+
+        (r_i, xsq, x, con)
+    }
 }
 
-fn synthesize<E: Engine, CS: ConstraintSystem<E>>(
-    cs: &mut CS,
-    z: &[AllocatedNum<E>],
-) -> Result<Vec<AllocatedNum<E>>, SynthesisError> {
-    // inputs
-    let state_0 = z[0].clone();
+// helper functions
+fn gen_eq_table(rs: &Vec<F>, qs: &Vec<usize>, last_q: &Vec<F>) -> Vec<F> {
+    let base: usize = 2;
+    let ell: usize = last_q.len();
 
-    // your logic here
+    let t_len = base.pow(ell as u32);
+    assert_eq!(rs.len(), qs.len() + 1);
 
-    Ok(vec![state_0])
+    let mut eq_t = vec![F::zero(); t_len];
+
+    for i in 0..qs.len() {
+        eq_t[qs[i]] += &rs[i];
+    }
+
+    for i in 0..eq_t.len() {
+        let mut term = rs[qs.len()].clone();
+
+        for j in (0..ell).rev() {
+            let xi = (i >> j) & 1;
+
+            term *= F::from(xi) * &last_q[j] + F::from(1 - xi) * (F::one() - &last_q[j]);
+        }
+        eq_t[i] += term;
+    }
+
+    eq_t
 }
+
+mod tests {}
