3e44983b66f101c888c2cef110ff198c2b6d5c31
jkwoods <woodsjk@seas.upenn.edu>
2024-09-16 15:13:16 -0400

    field elt conversion done

--- src/bellpepper.rs
+++ src/bellpepper.rs
@@ -1,4 +1,4 @@
-use ark_ff::Field as arkField;
+use ark_ff::{BigInteger, Field as arkField, PrimeField as arkPrimeField};
 use ark_relations::r1cs::{ConstraintMatrices, ConstraintSystemRef};
 use bellpepper::gadgets::Assignment;
 use bellpepper_core::{
@@ -6,7 +6,7 @@ use bellpepper_core::{
     num::AllocatedNum,
     ConstraintSystem, SynthesisError,
 };
-use ff::Field as novaField;
+use ff::{Field as novaField, PrimeField as novaPrimeField};
 use nova_snark::{
     provider::{PallasEngine, VestaEngine},
     traits::{circuit::StepCircuit, Engine, Group as novaGroup},
@@ -19,32 +19,40 @@ type EE2 = nova_snark::provider::ipa_pc::EvaluationEngine<E2>;
 type S1 = nova_snark::spartan::snark::RelaxedR1CSSNARK<E1, EE1>; // non-preprocessing SNARK
 type S2 = nova_snark::spartan::snark::RelaxedR1CSSNARK<E2, EE2>; // non-preprocessing SNARK
 
-#[derive(Clone, Debug)]
-struct FCircuit<G: novaGroup> {
-    //ark_matrices: Vec<ConstraintMatrices<F>>,
-    lcs: Vec<(
-        Vec<(G::Scalar, usize)>,
-        Vec<(G::Scalar, usize)>,
-        Vec<(G::Scalar, usize)>,
-    )>,
-    wit_assignments: Vec<G::Scalar>,
+fn ark_to_nova_field<A: arkPrimeField, N: novaPrimeField<Repr = [u8; 32]>>(ark_ff: &A) -> N {
+    // ark F -> ark BigInt
+    let b = ark_ff.into_bigint();
+
+    // BigInt -> bytes
+    let bytes = b.to_bytes_le();
+
+    // bytes -> nova F
+    N::from_repr(TryInto::<[u8; 32]>::try_into(bytes).unwrap()).unwrap()
 }
 
-impl<G: novaGroup> FCircuit<G> {
+#[derive(Clone, Debug)]
+struct FCircuit<N: novaPrimeField<Repr = [u8; 32]>> {
+    //ark_matrices: Vec<ConstraintMatrices<F>>,
+    lcs: Vec<(Vec<(N, usize)>, Vec<(N, usize)>, Vec<(N, usize)>)>,
+    wit_assignments: Vec<N>,
+    io_len: usize,
+}
+
+impl<N: novaPrimeField<Repr = [u8; 32]>> FCircuit<N> {
     // make circuits and witnesses for round i
-    pub fn new<F: arkField>(ark_cs_ref: ConstraintSystemRef<F>) -> (Self, Vec<G::Scalar>) {
+    pub fn new<A: arkPrimeField>(ark_cs_ref: ConstraintSystemRef<A>) -> (Self, Vec<N>) {
         ark_cs_ref.finalize();
         let ark_cs = ark_cs_ref.borrow().unwrap();
 
-        let io_assignments = ark_cs
+        let io_assignments: Vec<N> = ark_cs
             .instance_assignment
             .iter()
-            .map(|f| Self::ark_to_nova_field(f))
+            .map(|f| ark_to_nova_field(f))
             .collect(); // this is ouput and used as zi
         let wit_assignments = ark_cs
             .witness_assignment
             .iter()
-            .map(|f| Self::ark_to_nova_field(f))
+            .map(|f| ark_to_nova_field(f))
             .collect();
 
         let ark_matrices = ark_cs.to_matrices().unwrap();
@@ -53,15 +61,15 @@ impl<G: novaGroup> FCircuit<G> {
             lcs.push((
                 ark_matrices.a[i]
                     .iter()
-                    .map(|(val, index)| (Self::ark_to_nova_field(val), *index))
+                    .map(|(val, index)| (ark_to_nova_field(val), *index))
                     .collect(),
                 ark_matrices.b[i]
                     .iter()
-                    .map(|(val, index)| (Self::ark_to_nova_field(val), *index))
+                    .map(|(val, index)| (ark_to_nova_field(val), *index))
                     .collect(),
                 ark_matrices.c[i]
                     .iter()
-                    .map(|(val, index)| (Self::ark_to_nova_field(val), *index))
+                    .map(|(val, index)| (ark_to_nova_field(val), *index))
                     .collect(),
             ));
         }
@@ -70,26 +78,23 @@ impl<G: novaGroup> FCircuit<G> {
             FCircuit {
                 lcs,
                 wit_assignments,
+                io_len: io_assignments.len(),
             },
             io_assignments,
         )
     }
-
-    fn ark_to_nova_field<F: arkField>(ark_ff: &F) -> G::Scalar {
-        return G::Scalar::ZERO;
-    }
 }
 
-impl<G: novaGroup> StepCircuit<G::Scalar> for FCircuit<G> {
+impl<N: novaPrimeField<Repr = [u8; 32]>> StepCircuit<N> for FCircuit<N> {
     fn arity(&self) -> usize {
         2
     }
 
-    fn synthesize<CS: ConstraintSystem<G::Scalar>>(
+    fn synthesize<CS: ConstraintSystem<N>>(
         &self,
         cs: &mut CS,
-        z: &[AllocatedNum<G::Scalar>],
-    ) -> Result<Vec<AllocatedNum<G::Scalar>>, SynthesisError> {
+        z: &[AllocatedNum<N>],
+    ) -> Result<Vec<AllocatedNum<N>>, SynthesisError> {
         // io already allocated in z
 
         // allocate all wits
@@ -111,8 +116,34 @@ impl<G: novaGroup> StepCircuit<G::Scalar> for FCircuit<G> {
 #[cfg(test)]
 mod tests {
 
-    fn generate_arkworks_example() {}
+    use crate::bellpepper::*;
+    use ark_ff::BigInt;
+    use ff::PrimeField as novaPrimeField;
+    use nova_snark::traits::Group;
+    type NG = pasta_curves::pallas::Point;
+    type AF = ark_pallas::Fr;
 
+    #[test]
+    fn ff_convert() {
+        for v in vec![0, 1, 13, std::u64::MAX] {
+            let ark_val = AF::from(v);
+            let nova_val: <NG as Group>::Scalar = ark_to_nova_field(&ark_val);
+            assert_eq!(nova_val, <NG as Group>::Scalar::from(v));
+        }
+
+        // modulus
+        let mut biggest = AF::MODULUS;
+        biggest.sub_with_borrow(&BigInt::from(1u64));
+        let ark_biggest: AF = biggest.into();
+        let nova_val: <NG as Group>::Scalar = ark_to_nova_field(&ark_biggest);
+        assert_eq!(
+            nova_val,
+            <NG as Group>::Scalar::ZERO - <NG as Group>::Scalar::ONE
+        );
+    }
+
+    fn generate_arkworks_example() {}
+    /*
     fn run_nova() {
         let circuit_primary = FCircuit::new();
         let circuit_secondary = CubicCircuit::default();
@@ -193,5 +224,5 @@ mod tests {
             &[<E2 as Engine>::Scalar::ZERO],
         );
         assert!(res.is_ok());
-    }
+    }*/
 }
